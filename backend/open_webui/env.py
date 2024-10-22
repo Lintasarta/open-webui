8e6d5d30b42ca3bb70a43decfe18b4a0ac49f32b
Muhammad Raihan Afiandi <raihanafiandi@gmail.com>
2024-10-22 15:27:32 +0700

    initial config change

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -99,13 +99,13 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "DekaWicara")
+# if WEBUI_NAME != "Open WebUI":
+#     WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://www.shutterstock.com/image-vector/black-snake-logo-on-white-600nw-2324249251.jpg"
 
 
 ####################################
